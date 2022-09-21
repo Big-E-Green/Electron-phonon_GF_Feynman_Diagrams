6d29031086473e15475477ece23127f6b66e42d5
Big-E-Green <105460863+Big-E-Green@users.noreply.github.com>
2022-09-21 21:45:52 +0100

    fix
    made generators more efficient and streamlined permutation sequence

--- el_ph_generator.py
+++ el_ph_generator.py
@@ -1,71 +1,32 @@
 from itertools import permutations
 import timeit
 
-def genny(n):
-    One=[]
-    c=0
+def var_list(n):                        # var_list creates list of varibles 
+    vars=[]                             # i.e. 1,1',2,2',3,3',...,n,n'
+    c=1
     while n>=c:
         g=str(c)+str(c)
-        One.append(str(g))
+        vars.append(str(g))
         f=str(c)+'p'
-        One.append(str(f))
-        c=c+1
-    One.remove('0p')
-    One.remove('00')
-    return One
+        vars.append(str(f))
+        c+=1
+    return vars
 
-def all_diags(n):
-    s=timeit.default_timer()
-    lst = genny(n)
-    perm = permutations(lst)
-    hmm=[]
-    for i in list(perm):
-        hmm.append(i)
-    fin=[]
-    for i in hmm:
+def all_diagrams(n):                    # Finds all permutations of var_list
+    s=timeit.default_timer()            # then pairs them with the unaltered var_list
+    perm=permutations(var_list(n))      # yielding all possible diagrams
+    all_permutations=list(perm)
+    final=[]
+    for i in all_permutations:
         c=-1
         tmp2=[]
-        for k in genny(n):
+        for k in var_list(n):
             c=c+1
             tmp=[]
             tmp.append(k)
             tmp.append(i[c])
             tmp2.append(tmp)
-        fin.append(tmp2)
+        final.append(tmp2)
     st=timeit.default_timer()
     print('Time Generator:',st-s)
-    return fin
-
-def genall(n):
-    One=[]
-    c=0
-    while n>=c:
-        g=str(c)+str(c)
-        One.append(str(g))
-        f=str(c)+'p'
-        One.append(str(f))
-        c=c+1
-    One.remove('0p')
-    One.remove('00')
-    One.remove('1p')
-    One.remove('11')
-    chunkii=[]
-    for x in range(0, len(One), 2):
-        chunkii.append(One[x:x+2])
-    return chunkii
-
-def genall2(n):
-    One=[]
-    c=0
-    while n>=c:
-        g=str(c)+str(c)
-        One.append(str(g))
-        f=str(c)+'p'
-        One.append(str(f))
-        c=c+1
-    One.remove('0p')
-    One.remove('00')
-    chunkii=[]
-    for x in range(0, len(One), 2):
-        chunkii.append(One[x:x+2])
-    return chunkii
+    return final
