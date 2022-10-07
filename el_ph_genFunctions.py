3a610e43e5cae9e2ff7774273faf8e39e64c29ee
Big-E-Green <105460863+Big-E-Green@users.noreply.github.com>
2022-10-07 19:01:56 +0100

    add
    added additional functions for use in topological inequiv library

--- el_ph_genFunctions.py
+++ el_ph_genFunctions.py
@@ -1,18 +1,19 @@
+from copy import deepcopy
+
 def genall2(n):
     One=[]
     c=0
     while n>=c:
         g=str(c)+str(c)
-        One.append(str(g))
         f=str(c)+'p'
-        One.append(str(f))
+        One.extend([str(g),str(f)])
         c+=1
     One.remove('0p')
     One.remove('00')
-    chunkii=[]
+    chunk=[]
     for x in range(0, len(One), 2):
-        chunkii.append(One[x:x+2])
-    return chunkii
+        chunk.append(One[x:x+2])
+    return chunk
 
 def counterpart(n,inters):
     for i in inters:
@@ -72,13 +73,53 @@ def bigloopcheck(var,i):
                 i=[]
                 return False, hold
 
-def var_list_noone(n):                        # var_list creates list of varibles 
-    vars=[]                             # i.e. 1,1',2,2',3,3',...,n,n'
+def var_list_noone(n):
+    vars=[]
     c=2
     while n>=c:
         g=str(c)+str(c)
-        vars.append(str(g))
         f=str(c)+'p'
-        vars.append(str(f))
+        vars.extend([str(g),str(f)])
         c+=1
     return vars
+
+def pair_gen(n):
+    One=[]
+    c=2
+    while n>=c:
+        g=str(c)+str(c)
+        f=str(c)+'p'
+        One.extend([str(g),str(f)])
+        c+=1
+    chunked=[]
+    for x in range(0, len(One), 2):
+        chunked.append(One[x:x+2])
+    return chunked
+
+def swap(translation,numm):
+    cc=[]
+    for i in translation: 
+        c=-1
+        for j in numm:
+            c+=1
+            if j==i:
+                cc.append(c)
+                if len(cc)==2:
+                    return cc
+                    
+def reparam(trans,inp,numm):   
+    ii=deepcopy(inp)
+    for i in trans:
+        c=-1
+        for k in ii:
+            c+=1
+            c2=-1
+            for q in k:
+                c2+=1
+                if q==i[0]:
+                    ii[c][c2]=i[1]      
+                if q==i[1]:
+                    ii[c][c2]=i[0]
+        sw=swap(i,numm)
+        ii[sw[0]],ii[sw[1]]=ii[sw[1]],ii[sw[0]]
+    return ii
