1495c2f94079d9938b2acb076e6ca0d5dd7113e6
Big-E-Green <105460863+Big-E-Green@users.noreply.github.com>
2022-10-08 01:34:36 +0100

    update
    added annotations and turned into library

--- el_ph_topological_inequiv.py
+++ el_ph_topological_inequiv.py
@@ -3,9 +3,10 @@ import itertools
 
 def all_swapps(inp_n):
     all_trans=[]
+
     kk_indiv=[]
-    for i in pair_gen(inp_n):       
-        kk_indiv.append(i)           
+    for i in pair_gen(inp_n):              # gens individual k-->k' transforms
+        kk_indiv.append(i)   
     kk_multi=[]                     
     n_var=inp_n-1                   
     while n_var!=0:
@@ -13,7 +14,8 @@ def all_swapps(inp_n):
         kk_multi.append(permute)
         n_var-=1
     for i in kk_multi:
-        all_trans.extend(i) 
+        all_trans.extend(i)               # gens all k-->k' transf permutations
+
     perm=list(itertools.combinations(var_list_noone(inp_n),2))
     for i in list(kk_indiv):
         a=i[0]
@@ -28,7 +30,7 @@ def all_swapps(inp_n):
         counter2=counterpart(i[0][1],pair_gen(inp_n))
         if i[1][0]==counter1:
             if i[1][1]==counter2:
-                pair_swaps_indiv.append(i)            
+                pair_swaps_indiv.append(i)     # gens individual swap pairs
     pair_multi=[]                     
     n_var=inp_n-1                     
     while n_var!=0:
@@ -40,7 +42,8 @@ def all_swapps(inp_n):
                     pperm.append(k)
             pair_multi.append(pperm)
         n_var-=1
-    all_trans.extend(pair_multi)                    
+    all_trans.extend(pair_multi)     # gens all swap pair permutations
+
     kk_intpair_combs=[]
     for i in kk_multi:
         for j in i:
@@ -50,27 +53,5 @@ def all_swapps(inp_n):
                 tmp.extend(o)
                 kk_intpair_combs.append(tmp)
     for i in kk_intpair_combs:
-       all_trans.append(i)
+       all_trans.append(i)         # gens all permutations of k-->k' and swap pairs
     return all_trans
-
-def main():
-    n=3
-    num_string=[]
-    for i in genall2(n):
-        for j in i:                                              
-            num_string.append(j)
-    connected=[]
-    for i in all_diagrams(n):
-        result=connected_check(i,n)
-        if result==True:
-            connected.append(i)
-    for i in connected: 
-        for j in all_swapps(n): 
-            translated=reparam(j,i,num_string)
-            if translated in connected:
-                connected.remove(translated)
-    for i in connected:
-        print(i)
-
-if __name__ == "__main__":
-    main()
